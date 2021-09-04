59e644d2be33e10d4645ac8092ec266197bc5d00
Lautaro Nasello <lautaronasello@gmail.com>
2021-09-04 17:44:19 -0300

    extradosWebLanding

---
+++ src/Homepage.js
@@ -0,0 +1,12 @@
+import { Container, SimpleGrid, Box } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Homepage() {
+  return (
+    <Container h='100vh' bg='red ' maxW='container.lg'>
+      <SimpleGrid columns={[1, 2]} gap={['1rem', '3rem', '1rem']}>
+        
+      </SimpleGrid>
+    </Container>
+  );
+}
